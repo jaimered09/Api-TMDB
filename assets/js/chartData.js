d113f93a3b7e665f3f7d37bc7702778c6bdc7635
Jaime <jaramillonz16@gmail.com>
2023-07-14 17:02:48 -0400

    comentarios

--- assets/js/chartData.js
+++ assets/js/chartData.js
@@ -1,3 +1,4 @@
+// Js, con los metodos export de las constantes del gráfico
     export const movieRankings = {
     "Guardianes de la Galaxia 3": 82,
     "FastX": 74,
@@ -5,7 +6,7 @@
     "Los Caballeros del Zodiaco": 65,
     "San Andreas": 62,
   };
-
+// exportacion de las constantes para la opacidad del gráfico 1
     export const getDataColors = opacity => {
     const colors = ['#7448c2', '#21c0d7', '#d99e2b', '#cd3a81', '#9c99cc', '#e14eca', '#ffffff', '#ff0000', '#d6ff00', '#0038ff'];
     return colors.map(color => opacity ? `${color + opacity}` : color);
